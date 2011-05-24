b3682847ef6e01e5c55aeae02e02eba85b89a26c
Ben Ranker <branker@emory.edu>
2011-05-24 18:30:49 -0400

    cleanup setup.py, build dependencies

--- setup.py
+++ setup.py
@@ -1,44 +1,46 @@
-from distutils.command.build_py import build_py
-import os
-import sys
+#!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
+import eulfedora
 
-from eulfedora import __version__
+LONG_DESCRIPTION = None
+try:
+    # read the description if it's there
+    with open('README.rst') as desc_f:
+        LONG_DESCRIPTION = desc_f.read()
+except:
+    pass
 
-# fullsplit and packages calculation inspired by django setup.py
-
-def fullsplit(path):
-    result = []
-    while path:
-        path, tail = os.path.split(path)
-        result.append(tail)
-    result.reverse()
-    return result
-
-packages = []
-for path, dirs, files in os.walk(__file__):
-    if '.svn' in dirs:
-        dirs.remove('.svn')
-    if '__init__.py' in files:
-        packages.append(path.replace(os.path.sep, '.'))
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
 
 setup(
     name='eulfedora',
-    version=__version__,
+    version=eulfedora.__version__,
     author='Emory University Libraries',
     author_email='libsysdev-l@listserv.cc.emory.edu',
-    packages=packages,
-    package_dir={'': '.'},
+    url='https://github.com/emory-libraries/eulfedora',
+    license='Apache License, Version 2.0',
+    packages=find_packages(),
+
     install_requires=[
-#        'eulxml',
+        'eulxml',
         'rdflib>=3.0',
         'python-dateutil',
 	'poster',
         'pycrypto',
-        # soaplib still required for some fedora APIs
         'soaplib==0.8.1',
-        # pypi soaplib links are dead as of 20101201. use direct link for
-        # now: https://github.com/downloads/arskom/rpclib/soaplib-0.8.1.tar
     ],
+
+    description='Idiomatic access to digital objects in a Fedora Commons repository',
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
 )
