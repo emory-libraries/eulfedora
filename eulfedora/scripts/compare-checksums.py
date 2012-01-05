c10eca650e2390adf4703b84772d59b8caa685ef
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-01-05 13:29:47 -0500

    add apache license header; improved argument help/docs

--- eulfedora/scripts/compare-checksums.py
+++ eulfedora/scripts/compare-checksums.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# file eulfedora/scripts/compare-checksums.py
+# 
+#   Copyright 2012 Emory University Libraries
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 import argparse
 from collections import defaultdict
 from eulfedora.server import Repository
@@ -50,26 +66,34 @@ config.dictConfig(LOGGING)
 
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('pids', metavar='PID', nargs='*')
-    parser.add_argument('--fedora-root', dest='fedora_root', required=True)
-    parser.add_argument('--fedora-user', dest='fedora_user', default=None)
-#    parser.add_argument('--fedora-password', dest='fedora_password',
- #                       action=PasswordAction, default=None)
-
-    parser.add_argument('--fedora-password', dest='fedora_password',
-                        default=None)
-    
-
+    parser = argparse.ArgumentParser(description='''Validate datastream checksums
+    for Fedora repository content.  By default, iterates through all objects that
+    are findable by the findObjects REST API and checks all datastreams.
+    ''')
+    parser.add_argument('pids', metavar='PID', nargs='*',
+                        help='list specific pids to be checked (optional)')
+    parser.add_argument('--fedora-root', dest='fedora_root', required=True,
+                        help='URL for accessing fedora, e.g. http://localhost:8080/fedora/')
+    parser.add_argument('--fedora-user', dest='fedora_user', default=None, 
+                        help='Fedora username (requires permission to run compareDatastreamChecksum)')
+    # TODO: make both options available?
+    # prompt for password, but allow passing on command-line in dev/staging
+    # parser.add_argument('--fedora-password', dest='fedora_password',
+    #                      action=PasswordAction, default=None)
+    parser.add_argument('--fedora-password', dest='fedora_password', metavar='PASSWORD',
+                        default=None, help='Password for the specified Fedora user')
     args = parser.parse_args()
 
+    
     stats = defaultdict(int)
 
     repo = Repository(args.fedora_root, args.fedora_user, args.fedora_password)
 
     if args.pids:
+        # if pids were specified on the command line, use those
         objects = (repo.get_object(pid) for pid in args.pids)
     else:
+        # otherwise, process all find-able objects
         objects = repo.find_objects()
     
     for obj in objects:
@@ -81,10 +105,11 @@ def main():
                 stats['invalid_ds'] += 1
 
             # TODO: check if checksum type is DISABLED / checksum value none
+            
         stats['objects'] += 1
 
-    print "\nTested %(ds)d datastream(s) on %(objects)d object(s); found %(invalid_ds)d invalid checksum(s)" % \
-          stats
+    print '\nTested %(ds)d datastream(s) on %(objects)d object(s)' % stats
+    print 'Found %(invalid_ds)d invalid checksum(s)' % stats
 
 
 class PasswordAction(argparse.Action):
