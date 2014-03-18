5d0782b0444775d9d7876d73e8889b01573e883e
rlskoeser <rebecca.s.koeser@emory.edu>
2014-03-18 17:42:00 -0400

    configure validate-checksum script to be installed via setup.py [#58717416]

--- setup.py
+++ setup.py
@@ -60,5 +60,5 @@ setup(
     description='Idiomatic access to digital objects in a Fedora Commons repository',
     long_description=LONG_DESCRIPTION,
     classifiers=CLASSIFIERS,
-    scripts=['scripts/fedora-checksums', ],
+    scripts=['scripts/fedora-checksums', 'scripts/validate-checksums',],
 )
