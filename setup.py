a71e7d31de4c59949702e8d4fcd729c0df6e9aa0
rlskoeser <rebecca.s.koeser@emory.edu>
2014-12-17 17:03:13 -0500

    require unittest2 < 0.7 to avoid conflict with django test code

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
             'coverage',
             'Django<1.7',
             'mock',
+            'unittest2<0.7',  # optional testrunner in testutil
             'pyPdf',
             'progressbar',
         ]
