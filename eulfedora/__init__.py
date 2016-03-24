ac06f96a5637109f442d1dbc218325c848cb0cd5
rlskoeser <rebecca.s.koeser@emory.edu>
2016-03-24 16:56:49 -0400

    Bump version to 1.5.2

--- eulfedora/__init__.py
+++ eulfedora/__init__.py
@@ -15,7 +15,7 @@
 #   limitations under the License.
 
 
-__version_info__ = (1, 5, 1, None)
+__version_info__ = (1, 5, 2, None)
 
 # Dot-connect all but the last. Last is dash-connected if not None.
 __version__ = '.'.join([str(i) for i in __version_info__[:-1]])
