c6c1157465b0f8807adf4ba050af349c568c6ccb
rlskoeser <rebecca.s.koeser@emory.edu>
2016-07-14 17:05:43 -0400

    Tweak path for starting fedora

--- ci/start-fedora.sh
+++ ci/start-fedora.sh
@@ -2,7 +2,7 @@
 
 # shell script to start up continunous integration exist instance
 cd ${FCREPO_FOLDER}
-nohup start.sh &
+nohup jfs/start.sh &
 sleep 30
 cat nohup.out
 curl http://127.0.0.1:8080/fedora
