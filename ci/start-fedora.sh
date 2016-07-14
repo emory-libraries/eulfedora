c1e3136d0853b3ac10ebbde0eccddb7c88c60dff
rlskoeser <rebecca.s.koeser@emory.edu>
2016-07-14 16:45:12 -0400

    First pass at travis-ci with jfs fedora instance

---
+++ ci/start-fedora.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# shell script to start up continunous integration exist instance
+cd ${FCREPO_FOLDER}
+nohup start.sh &
+sleep 30
+cat nohup.out
+curl http://127.0.0.1:8080/fedora
