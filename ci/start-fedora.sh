7fefa0abacbdfdaccec17ec584e94958bf93cd56
rlskoeser <rebecca.s.koeser@emory.edu>
2016-07-14 17:46:15 -0400

    Try again on updating fedora starting path for travis-ci build

--- ci/start-fedora.sh
+++ ci/start-fedora.sh
@@ -2,7 +2,7 @@
 
 # shell script to start up continunous integration exist instance
 cd ${FCREPO_FOLDER}
-nohup jfs/start.sh &
+nohup ./start.sh &
 sleep 30
 cat nohup.out
 curl http://127.0.0.1:8080/fedora
