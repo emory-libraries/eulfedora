10ef1d447e3e5a315d58b8acc93a81b60db0bda1
Alex Zotov <alexandr.zotov@emory.edu>
2022-06-09 09:28:36 -0500

    Update util.py

--- eulfedora/util.py
+++ eulfedora/util.py
@@ -120,7 +120,7 @@ class ChecksumMismatch(RequestFailed):
 
 
 def parse_rdf(data, url, format=None):
-    fobj = StringIO(data)
+    fobj = BytesIO(data)
     rdfid = URIRef(url)
     graph = Graph(identifier=rdfid)
     if format is None:
